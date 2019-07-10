27c9ba08e36ec4cd5b70f8ab0e70e4712b48a28e
Inty Killa Raymi <cesar.cardozo.ud@gmail.com>
2019-07-10 20:52:27 +0200

    Main Menu Added

---
+++ Project Untity/EduCAR/Assets/Scripts/EscenaManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EscenaManager : MonoBehaviour
+{
+    public void mover_main()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void salir()
+    {
+        Application.Quit();
+    }
+}
